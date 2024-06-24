eef202c2dfa928aa695796b052bf5ee972988359
unknown <ytabib@ensup.eu>
2024-06-24 23:27:41 +0200

    Create HappyNumber.py

---
+++ Python/HappyNumber.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        seen = set()
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = sum(int(digit) ** 2 for digit in str(n))
+        return n == 1
